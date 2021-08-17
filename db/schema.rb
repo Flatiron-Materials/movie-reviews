eef3552cca47be29d7b5ca1688124106f0697156
Drew Keat <thedrewkeat@gmail.com>
2021-08-17 13:44:39 -0600

    generate Movie resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_194241) do
+ActiveRecord::Schema.define(version: 2021_08_17_194407) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "year"
+    t.date "release_date"
+    t.text "plot"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
