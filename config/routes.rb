5e82d123acfd6093420da63212f5a4c1c49bb20e
Drew Keat <thedrewkeat@gmail.com>
2021-08-17 22:10:33 -0600

    create login/signup form on root in static views, create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   resources :reviews
   resources :movies
   resources :users
 
   get '/', to:'static#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/', to:'static#login'
+  
 end
