d448a610bded26c0cc515594afeac87ecbc1cb60
Drew Keat <thedrewkeat@gmail.com>
2021-08-17 21:17:52 -0600

    create users_controller actions, and stub views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    def index
+    end
+
+    def def new
+        @user = User.new
+    end
+
+    def def create
+        @user = User.new(user_params)
+        if @user.save
+          flash[:success] = "User successfully created"
+          redirect_to @user
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+    def show
+    end
+
+    private
+
+    def user_params
+    end
 end
