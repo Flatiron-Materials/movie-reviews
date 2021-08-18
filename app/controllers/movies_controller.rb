d2d70eedf5145b73f4b350dc2a864a90d3293e3c
Drew Keat <thedrewkeat@gmail.com>
2021-08-18 12:58:13 -0600

    add index, create, and show actions to movies controller; add request_movie and create_movie methods to MoviesHelper; create index and show views for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,14 @@
 class MoviesController < ApplicationController
+
+    def index
+        @movies = Movie.all
+    end
+    
+    def create
+    end
+
+    def show
+        @movie = Movie.find(params[:id])
+    end
+
 end
