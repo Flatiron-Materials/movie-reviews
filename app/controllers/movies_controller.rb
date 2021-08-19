488ef875f7024548b8add9ddd505e421273d7575
Drew Keat <thedrewkeat@gmail.com>
2021-08-18 20:31:44 -0600

    add review movie form to user Show, create new movie view, add new and create methods to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,12 +3,59 @@ class MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end
+
+    def new
+        if params[:title]
+            @movies = []
+            search_movies(params[:title]).each do |movie_hash|
+                movie = new_movie(movie_hash)
+                @movies << movie
+            end
+        end
+    end
+    
     
     def create
+        @movie = new_movie(find_movie(params[:title]))
+        if @movie.save
+            redirect_to movie_path(@movie)
+        else
+            redirect_to user_path(current_user)
+        end
     end
 
     def show
         @movie = Movie.find(params[:id])
     end
 
+    private
+
+    def movie_params
+
+    end
+
+    def search_movies(movie_title)
+        request = RestClient.get "http://www.omdbapi.com/?apikey=c01df668&s=#{movie_title}&plot=full"
+        api_hash = JSON.parse(request.body)
+        search_results = api_hash["Search"]
+    end
+
+    def new_movie(api_hash)
+        attributes = {}
+        attributes[:title] = api_hash["Title"]
+        attributes[:year] = api_hash["Year"]
+        attributes[:rating] = api_hash["Rated"]
+        attributes[:release_date] = api_hash["Released"]
+        attributes[:runtime] = api_hash["Runtime"]
+        attributes[:genre] = api_hash["Genre"]
+        attributes[:plot] = api_hash["Plot"]
+        attributes[:awards] = api_hash["Awards"]
+        attributes[:poster_url] = api_hash["Poster"]
+        Movie.new(attributes)
+    end
+
+    def find_movie(movie_title)
+        request = RestClient.get "http://www.omdbapi.com/?apikey=c01df668&t=#{movie_title}&plot=full"
+        api_hash = JSON.parse(request.body)
+    end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,23 +1,2 @@
 module MoviesHelper
-
-    def request_movie(movie_title)
-        request = RestClient.get "http://www.omdbapi.com/?apikey=c01df668&t=#{movie_title}&plot=full"
-        api_hash = JSON.parse(request)
-    end
-
-    def create_movie(movie_title)
-        api_hash = request_movie(movie_title)
-        attributes = {}
-        attributes[:title] = api_hash["Title"]
-        attributes[:year] = api_hash["Year"]
-        attributes[:rating] = api_hash["Rated"]
-        attributes[:release_date] = api_hash["Released"]
-        attributes[:runtime] = api_hash["Runtime"]
-        attributes[:genre] = api_hash["Genre"]
-        attributes[:plot] = api_hash["Plot"]
-        attributes[:awards] = api_hash["Awards"]
-        attributes[:poster_url] = api_hash["Poster"]
-        Movie.create(attributes)
-    end
-
 end
